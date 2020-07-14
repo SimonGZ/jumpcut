d267edc33bebed6346a849b012dcab35fa22d8b6
Simon Ganz <simong@gmail.com>
2020-07-14 14:35:08 -0700

    Changing temporary values in fc.rs for testing

--- src/bin/fc.rs
+++ src/bin/fc.rs
@@ -2,6 +2,9 @@ use fountain_converter::parse;
 use std::fs;
 
 fn main() {
-    let scd = fs::read_to_string("benches/108.fountain").expect("file to load");
+    let scd = fs::read_to_string(
+        "/Users/simonganz/Programming/Rust/fountain_converter/benches/108.fountain",
+    )
+    .expect("file to load");
     parse(&scd);
 }
