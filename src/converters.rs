b4f35b1b2468ac2957c38ef8f59a0d565ea5b8cf
SimonGZ <dev@simonganz.com>
2021-04-07 09:32:55 -0700

    Refactoring FDX formatting into function
    Adding first test as well

--- src/converters.rs
+++ src/converters.rs
@@ -1,44 +1,12 @@
-use crate::{Element::*, Screenplay};
+use crate::{Element::*, Metadata, Screenplay};
 use handlebars::Handlebars;
 use std::collections::{HashMap, HashSet};
 
 impl Screenplay {
     pub fn to_final_draft(&mut self) -> String {
         let metadata = &mut self.metadata;
-        // Set Defaults
-        let mut scene_heading_style = "AllCaps".to_string();
-        let mut space_before_heading = "24".to_string();
-        let mut dialogue_spacing = "1".to_string();
-        let mut action_text_style = "".to_string();
-        let mut font_choice = "Courier Prime".to_string();
 
-        let fmt = metadata.get_mut("fmt");
-        match fmt {
-            None => (),
-            Some(opts_vec) => {
-                if let Some(opts_string) = opts_vec.first() {
-                    let lowercase = opts_string.to_lowercase();
-                    let options: HashSet<&str> = lowercase.split_whitespace().collect();
-                    for option in options {
-                        match option {
-                            "bsh" => scene_heading_style.push_str("+Bold"),
-                            "ush" => scene_heading_style.push_str("+Underline"),
-                            "acat" => action_text_style.push_str("AllCaps"),
-                            "ssbsh" => space_before_heading = "12".to_string(),
-                            "dsd" => dialogue_spacing = "2".to_string(),
-                            "cfd" => font_choice = "Courier Final Draft".to_string(),
-                            _ => (),
-                        }
-                    }
-                }
-            }
-        }
-
-        insert_helper(metadata, "scene-heading-style", scene_heading_style);
-        insert_helper(metadata, "space-before-heading", space_before_heading);
-        insert_helper(metadata, "dialogue-spacing", dialogue_spacing);
-        insert_helper(metadata, "action-text-style", action_text_style);
-        insert_helper(metadata, "font-choice", font_choice);
+        add_fdx_formatting(metadata);
 
         // Removing elements incompatible with Final Draft format
         self.elements.retain(|e| match e {
@@ -62,6 +30,69 @@ impl Screenplay {
     }
 }
 
-fn insert_helper(metadata: &mut HashMap<String, Vec<String>>, key: &str, value: String) -> () {
-    metadata.insert(key.to_string(), vec![value]);
+fn insert_helper(metadata: &mut HashMap<String, Vec<String>>, key: &str, value: &str) -> () {
+    metadata.insert(key.to_string(), vec![value.to_owned()]);
+}
+
+fn add_fdx_formatting(metadata: &mut Metadata) -> () {
+    // Set Defaults
+    let mut scene_heading_style = "AllCaps".to_string();
+    let mut space_before_heading = "24".to_string();
+    let mut dialogue_spacing = "1".to_string();
+    let mut action_text_style = "".to_string();
+    let mut font_choice = "Courier Prime".to_string();
+
+    let fmt = metadata.get_mut("fmt");
+    match fmt {
+        None => (),
+        Some(opts_vec) => {
+            if let Some(opts_string) = opts_vec.first() {
+                let lowercase = opts_string.to_lowercase();
+                let options: HashSet<&str> = lowercase.split_whitespace().collect();
+                for option in options {
+                    match option {
+                        "bsh" => scene_heading_style.push_str("+Bold"),
+                        "ush" => scene_heading_style.push_str("+Underline"),
+                        "acat" => action_text_style.push_str("AllCaps"),
+                        "ssbsh" => space_before_heading = "12".to_string(),
+                        "dsd" => dialogue_spacing = "2".to_string(),
+                        "cfd" => font_choice = "Courier Final Draft".to_string(),
+                        _ => (),
+                    }
+                }
+            }
+        }
+    }
+
+    insert_helper(metadata, "scene-heading-style", &scene_heading_style);
+    insert_helper(metadata, "space-before-heading", &space_before_heading);
+    insert_helper(metadata, "dialogue-spacing", &dialogue_spacing);
+    insert_helper(metadata, "action-text-style", &action_text_style);
+    insert_helper(metadata, "font-choice", &font_choice);
+}
+
+// * Tests
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_add_fdx_formatting() {
+        let mut metadata: Metadata = HashMap::new();
+        let mut expected: Metadata = HashMap::new();
+        [
+            ("scene-heading-style", "AllCaps"),
+            ("space-before-heading", "24"),
+            ("dialogue-spacing", "1"),
+            ("action-text-style", ""),
+            ("font-choice", "Courier Prime"),
+        ]
+        .iter()
+        .for_each(|pair| insert_helper(&mut expected, pair.0, pair.1));
+
+        add_fdx_formatting(&mut metadata);
+        assert_eq!(metadata, expected, "it should produce the correct defaults")
+    }
 }
