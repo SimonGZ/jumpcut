ad81014ac05de8285774892e79a51a8ad2d0e849
SimonGZ <dev@simonganz.com>
2025-07-10 21:50:44 -0700

    Changing benchmark time

--- benches/jumpcut_benchmarks.rs
+++ benches/jumpcut_benchmarks.rs
@@ -1,8 +1,10 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use jumpcut::parse;
+use std::time::Duration;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut parse_group = c.benchmark_group("Parse");
+    parse_group.measurement_time(Duration::from_secs(8));
 
     let scd_text = include_str!("108.fountain");
     let big_fish_text = include_str!("Big-Fish.fountain");
